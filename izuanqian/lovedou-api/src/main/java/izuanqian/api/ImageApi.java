cacddcf8062340cbc307f2d327906c0c3cb818d3
sanlion@vip.qq. com <sanlion@vip.qq. com>
2017-06-14 22:43:51 +0800

    locate

--- izuanqian/lovedou-api/src/main/java/izuanqian/api/ImageApi.java
+++ izuanqian/lovedou-api/src/main/java/izuanqian/api/ImageApi.java
@@ -5,17 +5,17 @@ import izuanqian.BizException;
 import izuanqian.FileService;
 import izuanqian.response.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -34,6 +34,7 @@ public class ImageApi {
     @Autowired
     private FileService fileService;
 
+    @ResponseBody
     @PostMapping
     @ApiOperation(value = "upload image, one by one", response = Api.Ok.class)
     public Api uploadFile(HttpServletRequest request) throws IOException {
